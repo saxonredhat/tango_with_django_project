accca620c3e4cbbdb7376ed42c7435a817ccb420
sheyinsong <saxon.rehdat@gmail.com>
2018-05-22 16:53:49 +0800

    add rango app

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'jm&=zj6l!xf14_3^v(q2xo=wr7y*tg2lz0l#5s67029o37s876'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rango',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
