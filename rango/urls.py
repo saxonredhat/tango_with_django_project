f79bc4dccb784361621e92c1a11baaaa4a01e9c5
sheyinsong <saxon.rehdat@gmail.com>
2018-05-23 10:56:27 +0800

    modify

--- rango/urls.py
+++ rango/urls.py
@@ -4,5 +4,8 @@ from rango import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^about/', views.about, name='about'),
     url(r'^category/(?P<slug>[\w\-]+)$', views.category_list, name='category_list'),
+    url(r'^add_category/$', views.add_category, name='add_category'),
+    url(r'^(?P<slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
 ]
--- rango/views.py
+++ rango/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render
-from rango.models import Category,Page
-
+from rango.models import Category, Page
+from rango.forms import CategoryForm, PageForm
 
 def index(request):
     #context_dict= {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
@@ -21,3 +21,38 @@ def category_list(request,slug):
     except Category.DoesNotExist:
         context_dict = {'pages': None, 'category': None}
     return render(request, 'rango/category_list.html', context=context_dict)
+
+
+def about(request):
+    return render(request, 'rango/about.html')
+
+
+def add_category(request):
+    form=CategoryForm()
+    if request.method == 'POST':
+        form=CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print(form.errors)
+    return render(request,'rango/add_category.html',{'form':form})
+
+
+def add_page(request, slug):
+    form = PageForm()
+    try:
+        category = Category.objects.get(slug=slug)
+    except Category.DoesNotExist:
+        return HttpResponse('category not exist!')
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.category = category
+            form.views = 0
+            form.save()
+            return category_list(request,slug)
+        else:
+            print(form.errors)
+    return render(request, 'rango/add_page.html', {'form': form,'category':category})
